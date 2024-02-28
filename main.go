4eb627c0bcd6d5bcfb93917422afbf29cfb17256
tenk9 <takuo6000@gmail.com>
2024-02-28 21:10:55 +0900

    Revert "add logger"
    This reverts commit 0f53d4aabee20003e9c193a706052557ae03ad36.

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
 	"InfoLinkAPI/src/models"
 	"InfoLinkAPI/src/routes"
 	_ "InfoLinkAPI/docs"
@@ -17,9 +16,7 @@ func main() {
     dsn := "root:root@(mysql-container:3306)/demo?charset=utf8&parseTime=True&loc=Local"
 
 	// データベースへの接続
-    db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
-		Logger: logger.Default.LogMode(logger.Info),
-	})
+    db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
     if err != nil {
         panic("データベースへの接続に失敗しました" + err.Error())
     }
